f1a8815d563559f1a3dd57962a9ab5f003a0d417
Jaidev sah <jaidevsah007@gmail.com>
2023-02-14 08:34:46 +0530

    Initial Commit

---
+++ Javaclass3.java
@@ -0,0 +1,156 @@
+public class Javaclass3 {
+
+    public static void main(String[] args) {
+
+        // What is programming in computer language ?
+
+        // => Programming is the process of designing and writing instructions for a computer to perform a
+        // specific task. The instructions are written in a programming language, which is a language designed
+        // to be understood by computers. A programmer writes a program, which is a set of instructions that the
+        // computer follows to complete a task. Programs can range from simple scripts to perform basic
+        // operations to complex software applications with multiple functions and features. Some common tasks
+        // performed by programs include performing mathematical calculations, displaying information on a
+        // screen, processing user input, storing and retrieving data, and communicating with other devices or
+        // systems. The purpose of programming is to automate tasks, solve problems, and create new and innovative
+        // applications to meet the needs of users and organizations.
+
+        //-----------------------------------------------
+
+        // What is Java ?
+
+        // => Java is a high-level programming language developed by Sun Microsystems (now owned by Oracle
+        // Corporation) in the mid-1990s. It is an object-oriented language that is designed to be portable
+        // across different platforms, which means that Java programs written on one platform can run on another
+        // platform without modification. Java is commonly used to build standalone applications, web
+        // applications, and mobile apps, as well as being a popular choice for developing large-scale enterprise
+        // applications. Java is also used in scientific computing, financial applications, and games
+        // development, among other areas.
+
+        //-----------------------------------------------
+
+        // why is java as compare to any other programming language ? OR Advantage.
+
+        // => Java has several advantages compared to other programming languages that have made it a popular
+        // choice for software development:
+
+        // 1. Platform independent: Java is designed to run on any device that has a Java Virtual Machine (JVM)
+        // installed, making it highly portable. This means that Java programs can run on any operating system,
+        // including Windows, macOS, and Linux.
+
+        // 2. Object-Oriented: Java is a pure object-oriented programming language, which means that all code
+        // written in Java is organized into objects, making it easier to write, maintain, and understand.
+
+        // 3. Rich API: Java has a large and comprehensive API, which provides a wide range of functionality,
+        // including I/O, networking, and XML parsing.
+
+        // 4. Secure: Java is designed with security in mind, making it a good choice for developing applications
+        // that handle sensitive information. Java's security features include automatic memory management and
+        // secure class loading.
+
+        // 5. Scalability: Java is highly scalable, making it suitable for large-scale enterprise applications.
+        // Java's scalability is due to its support for multithreading, which allows multiple tasks to be
+        // executed simultaneously.
+
+        // 6. Wide Adoption: Java is widely used by organizations, developers, and students, which means that
+        // there is a large community of developers who can provide support and resources, as well as a large
+        // pool of job opportunities for Java developers.
+
+        //--------------------------------------------------------------
+
+        // Introduction of Java
+
+        // Java is a high-level, general-purpose programming language that was first released in 1995 by Sun
+        // Microsystems (now owned by Oracle Corporation). It is an object-oriented language that is designed
+        // to be portable and platform-independent, which means that Java programs can run on any device that
+        // has a Java Virtual Machine (JVM) installed, regardless of the underlying hardware and operating system.
+        // Java is used to develop a wide range of applications, including standalone applications, web
+        // applications, mobile apps, games, and enterprise software. It has a large and comprehensive API,
+        // which provides a wide range of functionality, including I/O, networking, and XML parsing. Java is
+        // also known for its security features, which make it a good choice for developing applications that
+        // handle sensitive information. With its wide adoption, Java has a large and active community of
+        // developers, which provides support and resources for learning and using the language.
+
+        //------------------------------------------------------------
+
+        // What is compiler
+
+        // => A compiler in Java is a software program that takes the source code written in Java programming
+        // language and converts it into machine-readable code, which is then executed on a computer. The
+        // compiled code runs faster than the source code because it's in a format that can be executed directly
+        // by the computer's processor.
+
+        //The Java compiler is an important part of the Java Development Kit (JDK), which is the standard
+        // development environment for Java programming. The Java compiler is invoked from the command line
+        // using the javac command. It takes one or more source files as input and produces class files that
+        // contain the compiled code. These class files can then be executed using the Java Virtual Machine (JVM).
+        //
+        //In summary, the compiler in Java is responsible for taking the human-readable Java source code and
+        // transforming it into the machine-executable code that can be run on a computer.
+
+        //------------------------------------------------------------------
+
+        // What is JRE, JDK, JVM in java in simple language ?
+
+        // => Java has three important components that work together to run Java programs:
+
+        // 1. JRE (Java Runtime Environment): JRE is a software package that provides the minimum requirements to
+        // run Java applications and applets. It contains the Java Virtual Machine (JVM), core classes, and
+        // supporting files.
+
+        // 2. JDK (Java Development Kit): JDK is a software development kit that provides the tools and libraries
+        // necessary for developing Java applications. It includes the JRE, as well as the Java compiler,
+        // debuggers, and other tools that developers need to create and test Java applications.
+        //
+        // 3. JVM (Java Virtual Machine): JVM is the heart of the Java platform. It is responsible for executing
+        // Java bytecode, which is the compiled code of a Java program. The JVM interprets the bytecode, executes
+        // the instructions, and manages the memory used by the application.
+
+        // To run a Java program, you need to have JRE installed on your computer. If you want to develop Java
+        // applications, you need to have JDK installed, which includes the JRE and other tools necessary for
+        // development. JVM is an integral part of JRE and JDK and is used to execute the Java code.
+
+        //-----------------------------------------------------------------
+
+        // What is means by public class Name in java ?
+
+        // => In Java, the public keyword before the class keyword in a class declaration indicates that the
+        // class is a public class. This means that the class can be accessed from anywhere in the code,
+        // including other classes and packages.
+        //             The name after the class keyword is the name of the class, which is used to identify
+        //             the class. The name should follow the Java naming conventions, which specify that the
+        //             first letter of the name should be uppercase and subsequent words in the name should be
+        //             capitalized (also known as camelCase).
+
+        //-----------------------------------------------------
+
+        // What is means by public static void main (String [] args) in java ?
+
+        // In Java, "public static void main(String[] args)" is a method signature for the main method, which is
+        // the entry point of a Java application. The main method is called when the program starts executing.
+
+        // The "public" keyword before the "static" keyword indicates that the main method can be called from
+        // anywhere in the code. The static keyword means that the method belongs to the class and can be called
+        // without creating an instance of the class.
+
+        // The "void" keyword indicates that the main method does not return any value.
+
+        // The "main" method takes an array of strings as a parameter, represented by "String[] args". The "args"
+        // parameter is used to pass command line arguments to the program. The command line arguments are
+        // stored in the "args" array, which can be accessed and processed within the main method.
+
+        //Here's an example of a main method in Java:
+
+//        public class MainClass {
+//          public static void main(String[] args) {
+//            System.out.println("Hello, World!");
+//          }
+//        }
+        //In this example, the main method prints the message "Hello, World!" to the console.
+
+        //-------------------------------------------------------
+
+        // Loops in Java
+
+        // For-Loops, While-Loops, do-while Loops.
+    }
+}
